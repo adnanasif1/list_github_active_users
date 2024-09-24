59a72890ff8a1df468185a3f1128d61c4c2e9156
Adnan <adnan@adnan.com>
2024-09-24 12:36:44 +0000

    first commit

---
+++ list-users.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+
+function helper {
+    expected_no_of_args=2
+    if [ "$#" -ne "$expected_no_of_args" ]; then
+        echo "Please execute the script with the required arguments"
+   else
+	echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+	list_users_with_read_access
+    fi
+}
+
+# Main script
+
+helper "$@"
+
